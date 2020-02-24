db3a22c6d710ba7fb733bf671f47b355ead02949
Edgar Naranjo <edgar.naranjo>
2020-02-24 11:14:17 +0100

    Module iframe done

---
+++ README.md
@@ -0,0 +1,10 @@
+# website_iframe
+
+1 . Iframe for the virtual client office
+
+* Modulo para trabajar iframes en la Oficina Virtual de un cliente
+* Traducciones a español
+
+Para más información dirigirse al portal: https://github.com/todooweb
+
+Para dudas funcionales o soporte, dirigirse a: https://github.com/EdgarNaranjo/apps_todoo.git
---
+++ website_iframe/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Iframes Dashboard",
+    'summary': """Extend the Odoo Virtual Office with the option to add external iFrames.""",
+    'description': """
+        This module allows Clients to view external content through iframes by accessing their personal account of the 
+        Odoo Virtual Office. This can be used to add content from sources such as external report servers.
+    """,
+    'version': '10.0.0.0.1',
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+    'author': "ToDOO",
+    'website': "https://todooweb.es/",
+    'contributors': [
+        "Equipo Dev <devtodoo@gmail.com>",
+        "Edgar Naranjo <edgarnaranjof@gmail.com>",
+        "Tatiana Rosabal <tatianarosabal@gmail.com>",
+    ],
+    'support': 'devtodoo@gmail.com',
+    'depends': ['base',
+                'mail',
+                'website',
+                'website_portal_sale',
+                'website_portal',
+                ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/website_iframe.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
